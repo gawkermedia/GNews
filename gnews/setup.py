18d2f87379d2466ad418375cabe1050fa8cbac3d
ihab <irimawi@g-omedia.com>
2025-01-06 15:36:49 -0500

    Setup for installation

---
+++ gnews/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="GNews",
+    version="1.0.0",
+    description="A Python package for GNews integration.",
+    author="Ihab Rimawi",
+    packages=find_packages(),
+    install_requires=[
+        # Add dependencies here, e.g., 'requests', 'beautifulsoup4>=4.9.0'
+    ],
+)
